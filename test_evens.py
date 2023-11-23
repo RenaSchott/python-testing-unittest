a7d396516feb6648a06e2b224baa3e538fe436fd
R S <rena_schott@gmx.de>
2023-11-23 07:37:23 +0000

    Added new code

--- test_evens.py
+++ test_evens.py
@@ -1,7 +1,14 @@
 import unittest
+from evens import even_number_of_evens
 
 class TestEvens(unittest.TestCase):
-    pass
+    
+    #pass
+    #def test_function_return_True(self):
+    #   self.assertTrue(even_number_of_evens([]))
 
+    def test_throws_error_if_value_passed_in_is_not_list(self):
+        self.assertRaises(TypeError, even_number_of_evens, 4)
 
-unittest.main()
+if __name__= '__main__':
+    unittest.main()
